3c1b784bce561cbb915256124a4be8dfe9061c68
Phong Huynh <phonghdt@vng.com.vn>
2020-08-23 15:52:46 +0700

    User info main - bind

--- app/src/main/java/com/wind/deviantart/ui/user/UserFragment.kt
+++ app/src/main/java/com/wind/deviantart/ui/user/UserFragment.kt
@@ -5,18 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
+import androidx.databinding.BindingAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.observe
-import com.wind.deviantart.databinding.FragmentUserBinding
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import com.wind.deviantart.databinding.*
+import com.wind.model.UserInfo
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * Created by Phong Huynh on 8/17/2020
  */
 private const val EXTRA_DATA = "xData"
+
 @AndroidEntryPoint
-class UserFragment: Fragment() {
+class UserFragment : Fragment() {
     companion object {
         fun newInstance(userName: String): UserFragment {
             return UserFragment().apply {
@@ -38,7 +42,7 @@ class UserFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         viewBinding = FragmentUserBinding.inflate(inflater, container, false).apply {
-//            vm = vmUser
+            vm = vmUser
             lifecycleOwner = viewLifecycleOwner
         }
         return viewBinding.root
@@ -46,54 +50,137 @@ class UserFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        vmUser.apply {
-            userInfo.observe(viewLifecycleOwner) {
-
-            }
+        viewBinding.vPager.apply {
+            adapter = UserInfoAdapter()
         }
-//        viewBinding.vPager.apply {
-//            adapter =
-//        }
-//        TabLayoutMediator(viewBinding.tabLayout, viewBinding.vPager) { tab, pos ->
-            // do nothing
-//        }.attach()
     }
 }
 
-//class UserInfoAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-//
-//    var callback: Callback? = null
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-//        return when (viewType) {
-//
-//            else -> {
-//                throw IllegalStateException()
-//            }
-//        }
-//         ViewHolder(.inflate(LayoutInflater.from(parent.context), parent, false)
-//            .apply {
-//            }).apply {
-//            itemView.setOnClickListener { view ->
-//                val pos = bindingAdapterPosition
-//                if (pos >= 0) {
-//                    getItem(pos)?.let {
-//                        callback?.onClick(view, pos, it)
-//                    }
-//                }
-//            }
-//        }
-//    }
-//
-//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-//        val item = getItem(position)
-//        holder.binding.item = item
-//        holder.binding.executePendingBindings()
-//    }
-//
-//    interface Callback {
-//        fun onClick(view: View, pos: Int, item: Any)
-//    }
-//
-//    inner class ViewHolder(val binding: ) : RecyclerView.ViewHolder(binding.root)
-//}
+@BindingAdapter("data")
+fun ViewPager2.setData(userInfo: UserInfo?) {
+    userInfo?.let {
+        (adapter as? UserInfoAdapter)?.setData(it)
+    }
+}
+
+private const val TYPE_MAIN = 1
+private const val TYPE_PERSONAL = 2
+private const val TYPE_INTRODUCTION = 3
+private const val TYPE_SOCIAL = 4
+class UserInfoAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val types = mutableListOf<Int>()
+    private val data = mutableListOf<UserInfo>()
+
+    var callback: Callback? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            TYPE_MAIN -> {
+                UserMainViewHolder(ItemUserInfoMainBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+                    .apply {
+                        itemView.setOnClickListener { view ->
+                            val pos = bindingAdapterPosition
+                            if (pos >= 0) {
+                            }
+                        }
+                    }
+            }
+            TYPE_PERSONAL -> {
+                UserPersonalViewHolder(ItemUserInfoPersonalBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+                    .apply {
+                        itemView.setOnClickListener { view ->
+                            val pos = bindingAdapterPosition
+                            if (pos >= 0) {
+                            }
+                        }
+                    }
+            }
+            TYPE_INTRODUCTION -> {
+                UserIntroductionViewHolder(ItemUserInfoIntroductionBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+                    .apply {
+                        itemView.setOnClickListener { view ->
+                            val pos = bindingAdapterPosition
+                            if (pos >= 0) {
+                            }
+                        }
+                    }
+            }
+            TYPE_SOCIAL -> {
+                UserSocialViewHolder(ItemUserInfoSocialBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+                    .apply {
+                        itemView.setOnClickListener { view ->
+                            val pos = bindingAdapterPosition
+                            if (pos >= 0) {
+                            }
+                        }
+                    }
+            }
+            else -> {
+                throw IllegalStateException()
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return types[position]
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val item = data[position]
+        when (types[position]) {
+            TYPE_MAIN -> {
+                (holder as UserMainViewHolder).apply {
+                    binding.item = item
+                    binding.executePendingBindings()
+                }
+            }
+            TYPE_INTRODUCTION -> {
+                (holder as UserIntroductionViewHolder).apply {
+                    binding.item = item
+                    binding.executePendingBindings()
+                }
+            }
+            TYPE_PERSONAL -> {
+                (holder as UserPersonalViewHolder).apply {
+                    binding.item = item
+                    binding.executePendingBindings()
+                }
+            }
+            TYPE_SOCIAL -> {
+                (holder as UserSocialViewHolder).apply {
+                    binding.item = item
+                    binding.executePendingBindings()
+                }
+            }
+        }
+    }
+
+    fun setData(userInfo: UserInfo) {
+        types.clear()
+        data.clear()
+        types.add(TYPE_MAIN)
+        types.add(TYPE_INTRODUCTION)
+        types.add(TYPE_PERSONAL)
+        types.add(TYPE_SOCIAL)
+        data.add(userInfo)
+        data.add(userInfo)
+        data.add(userInfo)
+        data.add(userInfo)
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int {
+        return types.size
+    }
+
+    interface Callback {
+        fun onClick(view: View, pos: Int, item: Any)
+    }
+
+    private inner class UserMainViewHolder(val binding: ItemUserInfoMainBinding) : RecyclerView.ViewHolder(binding.root)
+    private inner class UserPersonalViewHolder(val binding: ItemUserInfoPersonalBinding) : RecyclerView.ViewHolder(binding.root)
+    private inner class UserIntroductionViewHolder(val binding: ItemUserInfoIntroductionBinding) : RecyclerView.ViewHolder(binding.root)
+    private inner class UserSocialViewHolder(val binding: ItemUserInfoSocialBinding) : RecyclerView.ViewHolder(binding.root)
+
+
+}
